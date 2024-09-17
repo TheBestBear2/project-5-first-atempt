2cade14f50eb22c3ecba318f3fbadc6fdf361089
jonathan <157562367+TheBestBear2@users.noreply.github.com>
2024-09-16 23:28:02 -0700

    Add files via upload
    added routes

---
+++ routes/pages.js
@@ -0,0 +1,24 @@
+
+const express = require('express')
+const loggedIn = require('../controllers/loggedIn')
+const { status } = require('init')
+const router = express.Router()
+
+router.get('/', loggedIn, (req, res) => {
+    if (req.user) {
+        res.render('index', { status: "LoggedIn", user: req.user })
+    } else {
+        res.render('index', { status: "no", user:"nothing" })
+    }
+    
+})
+
+router.get('/register', (req, res) => {
+    res.sendFile('register.html', {root: './public/'})
+})
+
+router.get('/login', (req, res) => {
+    res.sendFile('login.html', {root: './public/'})
+})
+
+module.exports = router
