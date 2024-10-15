4b8b4fc3f72e57bbdee12725bec8e5a1d5291188
jonathan bair <157562367+TheBestBear2@users.noreply.github.com>
2024-10-15 15:57:14 -0700

    Update register.js
    file updated

--- controllers/register.js
+++ controllers/register.js
@@ -3,14 +3,28 @@ const db = require('../routes/db-config')
 const bcrypt = require('bcryptjs')
 
 const register = async (req, res) => {
-    const {email, password: Npassword} = req.body
+    console.log('register api')
+    console.log(req)
+    //const {email, password: Npassword} = req.body
+    const Npassword = req.body.password;
+    const email = req.body.email;
+    console.log(email)
+    console.log(Npassword)
     if (!email || !Npassword) return res.json({ status: 'error', error:'Please enter your email and password'})
         else{
-            
+            console.log('in the query to fetch')
             db.query('SELECT email FROM users WHERE email = ?', [email], async(err, result) => {
                 if (err) throw err;
-                if (result[0]) return res.json({ status: 'error', error:'email has aleady been registed'})
-                else {
+               if(result.length > 0)
+               {
+                if (result[0].email != 0 || result == []) {
+                    console.log(result[0].email != 0)
+                    return res.json({ status: 'error', error:'email has aleady been registed'})
+               }   
+               }
+             
+                    
+                    else {
                     const password = await bcrypt.hash(Npassword, 8)
                     
                     db.query('INSERT INTO users SET ?', { email: email, password: password }, (error, result) => {
